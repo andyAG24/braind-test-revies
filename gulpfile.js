9453a15104d1da6582800085a59ceeb0455c7830
andyAG24 <andy.ag.24@gmail.com>
2020-06-20 17:39:35 +0300

    Added title, notice, table caption. Tuned Gulp tasks. Reformed directories

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,50 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var config = {
+  srcDir: './src/styles',
+  sassPattern: '*.scss',
+  clean: 'build/css',
+  watch: {
+    html: './build/index.html',
+    sass: './src/styles/main.scss'
+  }
+}
 
-gulp.task('kek', function () {
-    console.log('kek');
-})
+var gulp = require('gulp'), 
+  sass = require('gulp-sass');
+const concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps'),
+  clean = require('gulp-clean'),
+  watch = require('gulp-watch'),
+  del = require('del');
+var browserSync = require('browser-sync').create();
 
-gulp.task('scss', function() {
-  return gulp.src('styles/*.scss')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('destination'));
+gulp.task('sass', function () {
+  return gulp.src(config.srcDir + '/' + config.sassPattern)
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('main.css'))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./build/css'))
+  .pipe(browserSync.reload({stream: true}))
+});
+ 
+gulp.task('watch', function() {
+  gulp.watch(['build/index.html', 'src/blocks/**/*.scss'], gulp.series(['clean', 'sass', 'browser-sync']));
 });
 
-// gulp.watch('styles/*.scss', ['scss']);
+gulp.task('clean', function(done) {
+  del(config.clean);
+  done();
+});
+
+gulp.task('browser-sync', function(done) {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    },
+    notify: false
+  });
+
+  browserSync.watch('./build').on('change', browserSync.reload);
+
+  done();
+});
